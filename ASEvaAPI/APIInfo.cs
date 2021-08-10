d92f67dbc5d4646249d0463b57c2930098ac4c86
Ken <sdken777@gmail.com>
2021-08-10 09:31:29 +0800

    2.0.8.0:新增ResourceLoader.LoadText

--- ASEvaAPI/APIInfo.cs
+++ ASEvaAPI/APIInfo.cs
@@ -7,7 +7,7 @@
  * \n
  * 另外，插件中使用的样本相关定义参考 ASEva.Samples ；图表报告相关定义参考 ASEva.Graph \n
  * \n
- * 本文档对应API版本：2.0.7
+ * 本文档对应API版本：2.0.8
  */
 
 using System;
@@ -16,7 +16,7 @@ using System.Reflection;
 namespace ASEva
 {
     /// <summary>
-    /// version=2.0.7
+    /// version=2.0.8
     /// </summary>
     public class APIInfo
     {
@@ -26,7 +26,7 @@ namespace ASEva
         /// <returns>API版本</returns>
         public static Version GetAPIVersion()
         {
-            return new Version(2, 0, 7, 0); // 统一由Handler实现执行所有操作
+            return new Version(2, 0, 8, 0); // 新增ResourceLoader.LoadText
         }
 
         /// <summary>
--- ASEvaAPI/Utility/ResourceLoader.cs
+++ ASEvaAPI/Utility/ResourceLoader.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Reflection;
 
 namespace ASEva.Utility
@@ -21,6 +22,19 @@ namespace ASEva.Utility
             var data = new byte[instream.Length];
             instream.Read(data, 0, data.Length);
             return data;
+        }
+
+        /// <summary>
+        /// (api:app=2.0.8) 按指定名称读取资源，并转为文本
+        /// </summary>
+        /// <param name="name">资源名称</param>
+        /// <returns>文本数据，若找不到资源则返回null</returns>
+        public static String LoadText(String name)
+        {
+            var data = Load(name);
+            if (data == null) return null;
+
+            return Encoding.UTF8.GetString(data);
         }
     }
 }
