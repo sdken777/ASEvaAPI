2944e58d9fcdfbd605f5356524f5d660398eadee
Ken <sdken777@gmail.com>
2022-01-06 11:27:05 +0800

    common-2.2.4:新增CommonCameraMeta.K3, K4, K5, K6

--- ASEvaAPI/APIInfo.cs
+++ ASEvaAPI/APIInfo.cs
@@ -7,7 +7,7 @@
  * \n
  * 另外，插件中使用的样本相关定义参考 ASEva.Samples ；图表报告相关定义参考 ASEva.Graph \n
  * \n
- * 本文档对应API版本：2.2.3
+ * 本文档对应API版本：2.2.4
  */
 
 using System;
@@ -16,7 +16,7 @@ using System.Diagnostics;
 namespace ASEva
 {
     /// <summary>
-    /// version=2.2.3
+    /// version=2.2.4
     /// </summary>
     public class APIInfo
     {
@@ -26,7 +26,7 @@ namespace ASEva
         /// <returns>API版本</returns>
         public static Version GetAPIVersion()
         {
-            return new Version(2, 2, 3, 0); // 新增Agency.GetEventTypeNames, Agency.ParseBusMessage
+            return new Version(2, 2, 4, 0); // 新增CommonCameraMeta.K3, K4, K5, K6
         }
 
         /// <summary>
