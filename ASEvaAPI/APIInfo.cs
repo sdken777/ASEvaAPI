1d519f71edfdc198dc51428cd322e6069693e380
Ken <sdken777@gmail.com>
2022-05-17 11:24:39 +0800

    common-2.6.4.0:新增AudioFrameSample.MD5

--- ASEvaAPI/APIInfo.cs
+++ ASEvaAPI/APIInfo.cs
@@ -7,7 +7,7 @@
  * \n
  * 另外，插件中使用的样本相关定义参考 ASEva.Samples ；图表报告相关定义参考 ASEva.Graph \n
  * \n
- * 本文档对应API版本：2.6.3
+ * 本文档对应API版本：2.6.4
  */
 
 using System;
@@ -16,7 +16,7 @@ using System.Diagnostics;
 namespace ASEva
 {
     /// <summary>
-    /// version=2.6.3
+    /// version=2.6.4
     /// </summary>
     public class APIInfo
     {
@@ -26,7 +26,7 @@ namespace ASEva
         /// <returns>API版本</returns>
         public static Version GetAPIVersion()
         {
-            return new Version(2, 6, 3, 0); // 新增Agency.AddBusProtocolFile(filePath)和AddBusProtocolResult
+            return new Version(2, 6, 4, 0); // 新增AudioFrameSample.MD5
         }
 
         /// <summary>
--- ASEvaAPI/Samples/AudioFrameSample.cs
+++ ASEvaAPI/Samples/AudioFrameSample.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ASEva.Utility;
 
 namespace ASEva.Samples
 {
@@ -10,6 +11,28 @@ namespace ASEva.Samples
         /// <summary>
         /// 音频帧数据
         /// </summary>
-        public short[] Data { get; set; }
+        public short[] Data
+        {
+            get { return data; }
+            set
+            {
+                data = value;
+                if (data == null) MD5 = null;
+                else
+                {
+                    var buffer = new byte[data.Length * 2];
+                    if (buffer.Length > 0) Buffer.BlockCopy(data, 0, buffer, 0, buffer.Length);
+                    MD5 = MD5Calculator.Calculate(buffer);
+                }
+            }
+        }
+
+        /// <summary>
+        /// (api:app=2.6.4) 音频帧数据的MD5
+        /// </summary>
+        /// <value></value>
+        public String MD5 { get; private set; }
+
+        private short[] data;
     }
 }
