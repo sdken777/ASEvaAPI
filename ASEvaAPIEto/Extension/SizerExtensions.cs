1464506e2d613b4427add25571b2490e61fb5c0c
Ken <sdken777@gmail.com>
2022-06-06 11:24:31 +0800

    eto-2.5.1.0:新增SizeExtensions.GetLogicalSize, GetLogicalPoint

--- ASEvaAPIEto/Extension/SizerExtensions.cs
+++ ASEvaAPIEto/Extension/SizerExtensions.cs
@@ -5,36 +5,36 @@ using Eto.Drawing;
 namespace ASEva.UIEto
 {
     /// <summary>
-    /// (api:eto=2.0.0) 提供非默认DPI尺寸转换的扩展
+    /// (api:eto=2.0.0) 提供逻辑尺寸与实际像素值转换的扩展
     /// </summary>
     public static class SizerExtensions
     {
         /// <summary>
-        /// 转换像素值（单值）
+        /// 转换为实际像素值（单值）
         /// </summary>
         /// <param name="control">控件</param>
-        /// <param name="size">默认DPI下的像素值</param>
-        /// <returns>任意DPI下的像素值</returns>
+        /// <param name="size">逻辑尺寸</param>
+        /// <returns>实际像素值</returns>
         public static int Sizer(this Control control, int size)
         {
             int outSize = size;
-            var scale = PixelScale;
+            var scale = Pixel.Scale;
             if (scale != 1) outSize = (int)(size * scale);
             return Math.Max(1, outSize);
         }
 
         /// <summary>
-        /// 转换像素值（二维）
+        /// 转换为实际像素值（二维）
         /// </summary>
         /// <param name="control">控件</param>
-        /// <param name="width">默认DPI下的宽度像素值</param>
-        /// <param name="height">默认DPI下的高度像素值</param>
-        /// <returns>任意DPI下的像素尺寸</returns>
+        /// <param name="width">逻辑宽度</param>
+        /// <param name="height">逻辑高度</param>
+        /// <returns>实际像素值</returns>
         public static Size Sizer(this Control control, int width, int height)
         {
             int outWidth = width;
             int outHeight = height;
-            var scale = PixelScale;
+            var scale = Pixel.Scale;
             if (scale != 1)
             {
                 outWidth = (int)(width * scale);
@@ -44,66 +44,78 @@ namespace ASEva.UIEto
         }
 
         /// <summary>
-        /// (api:eto=2.3.5) 获取控件宽度
+        /// (api:eto=2.3.5) 获取控件逻辑宽度
         /// </summary>
         /// <param name="control">控件</param>
-        /// <returns>控件宽度</returns>
+        /// <returns>逻辑宽度</returns>
         public static int GetLogicalWidth(this Control control)
         {
-            var scale = PixelScale;
+            var scale = Pixel.Scale;
             if (scale != 1) return (int)(control.Width / scale);
             else return control.Width;
         }
 
         /// <summary>
-        /// (api:eto=2.3.5) 获取控件高度
+        /// (api:eto=2.3.5) 获取控件逻辑高度
         /// </summary>
         /// <param name="control">控件</param>
-        /// <returns>控件高度</returns>
+        /// <returns>逻辑高度</returns>
         public static int GetLogicalHeight(this Control control)
         {
-            var scale = PixelScale;
+            var scale = Pixel.Scale;
             if (scale != 1) return (int)(control.Height / scale);
             else return control.Height;
         }
 
         /// <summary>
-        /// 设置控件宽度
+        /// (api:eto=2.5.1) 获取控件逻辑尺寸
         /// </summary>
         /// <param name="control">控件</param>
-        /// <param name="width">设置宽度</param>
+        /// <returns>逻辑尺寸</returns>
+        public static Size GetLogicalSize(this Control control)
+        {
+            var scale = Pixel.Scale;
+            if (scale != 1) return new Size((int)(control.Width / scale), (int)(control.Height / scale));
+            else return control.Size;
+        }
+
+        /// <summary>
+        /// 设置控件逻辑宽度
+        /// </summary>
+        /// <param name="control">控件</param>
+        /// <param name="width">逻辑宽度</param>
         public static void SetLogicalWidth(this Control control, int width)
         {
             int setWidth = width;
-            var scale = PixelScale;
+            var scale = Pixel.Scale;
             if (scale != 1) setWidth = (int)(width * scale);
             control.Width = Math.Max(1, setWidth);
         }
 
         /// <summary>
-        /// 设置控件高度
+        /// 设置控件逻辑高度
         /// </summary>
         /// <param name="control">控件</param>
-        /// <param name="height">设置高度</param>
+        /// <param name="height">逻辑高度</param>
         public static void SetLogicalHeight(this Control control, int height)
         {
             int setHeight = height;
-            var scale = PixelScale;
+            var scale = Pixel.Scale;
             if (scale != 1) setHeight = (int)(height * scale);
             control.Height = Math.Max(1, setHeight);
         }
 
         /// <summary>
-        /// 设置控件尺寸
+        /// 设置控件逻辑尺寸
         /// </summary>
         /// <param name="control">控件</param>
-        /// <param name="width">设置宽度</param>
-        /// <param name="height">设置高度</param>
+        /// <param name="width">逻辑宽度</param>
+        /// <param name="height">逻辑高度</param>
         public static void SetLogicalSize(this Control control, int width, int height)
         {
             int setWidth = width;
             int setHeight = height;
-            var scale = PixelScale;
+            var scale = Pixel.Scale;
             if (scale != 1)
             {
                 setWidth = (int)(width * scale);
@@ -113,7 +125,19 @@ namespace ASEva.UIEto
         }
 
         /// <summary>
-        /// 像素单位比例，用于优化在非默认DPI下的显示
+        /// (api:eto=2.5.1) 获取鼠标事件中鼠标位置的逻辑坐标
+        /// </summary>
+        /// <param name="args">鼠标事件</param>
+        /// <returns>鼠标位置的逻辑坐标</returns>
+        public static PointF GetLogicalPoint(this MouseEventArgs args)
+        {
+            var scale = Pixel.Scale;
+            if (scale != 1) return new PointF(args.Location.X / scale, args.Location.Y / scale);
+            else return args.Location;
+        }
+
+        /// <summary>
+        /// 像素单位比例
         /// </summary>
         public static float PixelScale
         {
