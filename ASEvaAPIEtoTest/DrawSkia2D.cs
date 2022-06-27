1ff0b6fb175b91412ee66499c9fce22e0b00c2e1
Ken <sdken777@gmail.com>
2022-06-27 15:44:34 +0800

    Skia测试启用反锯齿

--- ASEvaAPIEtoTest/DrawSkia2D.cs
+++ ASEvaAPIEtoTest/DrawSkia2D.cs
@@ -17,16 +17,16 @@ namespace ASEvaAPIEtoTest
             {
                 var c = args.Canvas;
                 c.Clear(SKColors.LightYellow);
-                c.DrawLine(10, 100, 190, 100, new SKPaint{ Color = SKColors.Black });
-                c.DrawLine(10, 120, 190, 120, new SKPaint{ Color = SKColors.Black });
-                c.DrawLine(100, 10, 100, 190, new SKPaint{ Color = SKColors.Black });
-                c.DrawLine(20, 110, 90, 110, new SKPaint{ Color = SKColors.Red, StrokeWidth = 20, StrokeCap = SKStrokeCap.Square });
-                c.DrawText(t["draw-text"], 100, 120, new SKPaint(c.GetDefaultFont()) { Color = SKColors.Black });
+                c.DrawLine(10, 100, 190, 100, new SKPaint{ Color = SKColors.Black, IsAntialias = true });
+                c.DrawLine(10, 120, 190, 120, new SKPaint{ Color = SKColors.Black, IsAntialias = true });
+                c.DrawLine(100, 10, 100, 190, new SKPaint{ Color = SKColors.Black, IsAntialias = true });
+                c.DrawLine(20, 110, 90, 110, new SKPaint{ Color = SKColors.Red, StrokeWidth = 20, StrokeCap = SKStrokeCap.Square, IsAntialias = true });
+                c.DrawText(t["draw-text"], 100, 120, new SKPaint(c.GetDefaultFont()) { Color = SKColors.Black, IsAntialias = true });
                 c.DrawImage(CommonImage.LoadResource("camera.png").ToSKImage(), 80, 80);
-                c.DrawArc(new SKRect(10, 10, 190, 190), -90, 270, true, new SKPaint{ Color = new SKColor(0, 128, 0, 128), StrokeWidth = 2 });
-                c.DrawLine(10, 210, 190, 215, new SKPaint{ Color = SKColors.Black });
-                c.DrawLine(10, 230, 190, 235, new SKPaint{ Color = SKColors.Black });
-                c.DrawText(t["draw-skia-anti-alias"], 100, 230, new SKPaint(c.GetDefaultFont()) { Color = SKColors.Black, TextAlign = SKTextAlign.Center });
+                c.DrawArc(new SKRect(10, 10, 190, 190), -90, 270, true, new SKPaint{ Color = new SKColor(0, 128, 0, 128), StrokeWidth = 2, IsAntialias = true });
+                c.DrawLine(10, 210, 190, 215, new SKPaint{ Color = SKColors.Black, IsAntialias = true });
+                c.DrawLine(10, 230, 190, 235, new SKPaint{ Color = SKColors.Black, IsAntialias = true });
+                c.DrawText(t["draw-skia-anti-alias"], 100, 230, new SKPaint(c.GetDefaultFont()) { Color = SKColors.Black, TextAlign = SKTextAlign.Center, IsAntialias = true });
             };
 
             Closing += delegate
