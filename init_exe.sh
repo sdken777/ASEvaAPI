218b6ec2494657fe96d95b4c6cfe8ba9ada6910e
Ken <sdken777@gmail.com>
2022-07-20 10:04:35 +0800

    init_exe脚本初始化第三方库

--- init_exe.sh
+++ init_exe.sh
@@ -15,5 +15,65 @@ chmod -v +x "$CUR_DIR"/binxa/ASEvaAPIEtoTest
 chmod -v +x "$CUR_DIR"/binx/TextResourceEditor
 chmod -v +x "$CUR_DIR"/binxa/TextResourceEditor
 
+cp -vf "$CUR_DIR"/3party/common/*.* "$CUR_DIR"/bin64/
+cp -vf "$CUR_DIR"/3party/common/*.* "$CUR_DIR"/bin64/debug/
+cp -vf "$CUR_DIR"/3party/common/*.* "$CUR_DIR"/binx/
+cp -vf "$CUR_DIR"/3party/common/*.* "$CUR_DIR"/binx/debug/
+cp -vf "$CUR_DIR"/3party/common/*.* "$CUR_DIR"/binxa/
+cp -vf "$CUR_DIR"/3party/common/*.* "$CUR_DIR"/binxa/debug/
+cp -vf "$CUR_DIR"/3party/common/*.* "$CUR_DIR"/binm/
+cp -vf "$CUR_DIR"/3party/common/*.* "$CUR_DIR"/binm/debug/
+
+cp -vf "$CUR_DIR"/3party/corewf-wpf/*.* "$CUR_DIR"/bin64/
+cp -vf "$CUR_DIR"/3party/corewf-wpf/*.* "$CUR_DIR"/bin64/debug/
+
+cp -vf "$CUR_DIR"/3party/eto/*.* "$CUR_DIR"/bin64/
+cp -vf "$CUR_DIR"/3party/eto/*.* "$CUR_DIR"/bin64/debug/
+cp -vf "$CUR_DIR"/3party/eto/*.* "$CUR_DIR"/binx/
+cp -vf "$CUR_DIR"/3party/eto/*.* "$CUR_DIR"/binx/debug/
+cp -vf "$CUR_DIR"/3party/eto/*.* "$CUR_DIR"/binxa/
+cp -vf "$CUR_DIR"/3party/eto/*.* "$CUR_DIR"/binxa/debug/
+cp -vf "$CUR_DIR"/3party/eto/*.* "$CUR_DIR"/binm/
+cp -vf "$CUR_DIR"/3party/eto/*.* "$CUR_DIR"/binm/debug/
+
+cp -vf "$CUR_DIR"/3party/eto-doc/*.* "$CUR_DIR"/bin64/
+cp -vf "$CUR_DIR"/3party/eto-doc/*.* "$CUR_DIR"/bin64/debug/
+cp -vf "$CUR_DIR"/3party/eto-doc/*.* "$CUR_DIR"/binx/
+cp -vf "$CUR_DIR"/3party/eto-doc/*.* "$CUR_DIR"/binx/debug/
+cp -vf "$CUR_DIR"/3party/eto-doc/*.* "$CUR_DIR"/binxa/
+cp -vf "$CUR_DIR"/3party/eto-doc/*.* "$CUR_DIR"/binxa/debug/
+cp -vf "$CUR_DIR"/3party/eto-doc/*.* "$CUR_DIR"/binm/
+cp -vf "$CUR_DIR"/3party/eto-doc/*.* "$CUR_DIR"/binm/debug/
+
+cp -vf "$CUR_DIR"/3party/gtk/*.* "$CUR_DIR"/binx/
+cp -vf "$CUR_DIR"/3party/gtk/*.* "$CUR_DIR"/binx/debug/
+
+cp -vf "$CUR_DIR"/3party/gtk/*.* "$CUR_DIR"/binxa/
+cp -vf "$CUR_DIR"/3party/gtk/*.* "$CUR_DIR"/binxa/debug/
+
+cp -vf "$CUR_DIR"/3party/linux-arm64/*.* "$CUR_DIR"/binxa/
+cp -vf "$CUR_DIR"/3party/linux-arm64/*.* "$CUR_DIR"/binxa/debug/
+
+cp -vf "$CUR_DIR"/3party/linux-arm64-skia-native/*.* "$CUR_DIR"/binxa/
+cp -vf "$CUR_DIR"/3party/linux-arm64-skia-native/*.* "$CUR_DIR"/binxa/debug/
+
+cp -vf "$CUR_DIR"/3party/linux-x64/*.* "$CUR_DIR"/binx/
+cp -vf "$CUR_DIR"/3party/linux-x64/*.* "$CUR_DIR"/binx/debug/
+
+cp -vf "$CUR_DIR"/3party/linux-x64-skia-native/*.* "$CUR_DIR"/binx/
+cp -vf "$CUR_DIR"/3party/linux-x64-skia-native/*.* "$CUR_DIR"/binx/debug/
+
+cp -vf "$CUR_DIR"/3party/macos/*.* "$CUR_DIR"/binm/
+cp -vf "$CUR_DIR"/3party/macos/*.* "$CUR_DIR"/binm/debug/
+
+cp -vf "$CUR_DIR"/3party/macos-skia-native/*.* "$CUR_DIR"/binm/
+cp -vf "$CUR_DIR"/3party/macos-skia-native/*.* "$CUR_DIR"/binm/debug/
+
+cp -vf "$CUR_DIR"/3party/monomac/*.* "$CUR_DIR"/binm/
+cp -vf "$CUR_DIR"/3party/monomac/*.* "$CUR_DIR"/binm/debug/
+
+cp -vf "$CUR_DIR"/3party/windows-skia-native/*.* "$CUR_DIR"/bin64/
+cp -vf "$CUR_DIR"/3party/windows-skia-native/*.* "$CUR_DIR"/bin64/debug/
+
 echo "Done. (ASEvaAPI/init_exe.sh)"
 sleep 1
