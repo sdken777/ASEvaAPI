9e86a3dd821fabac6f8b28afeb1bf5998c0c5d3f
Ken <sdken777@gmail.com>
2023-03-13 14:04:05 +0800

    eto-2.10.0:新增SimpleTreeView 测试程序增加列表的条目个数

---
+++ ASEvaAPIEto/SimpleTreeView.cs
@@ -0,0 +1,317 @@
+using System;
+using System.Collections.Generic;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace ASEva.UIEto
+{
+    /// <summary>
+    /// (api:eto=2.10.0) 简易树状视图
+    /// </summary>
+    public class SimpleTreeView : Panel, SimpleTreeViewCallback
+    {
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public SimpleTreeView()
+        {
+            if (Factory == null) Factory = new DefaultSimpleTreeViewFactory();
+
+            Control etoControl = null;
+            Factory.CreateSimpleTreeViewBackend(this, out etoControl, out backend);
+            if (etoControl != null) Content = etoControl;
+        }
+
+        /// <summary>
+        /// 设置树状视图内容
+        /// </summary>
+        /// <param name="rootNodes">内容的根节点集合</param>
+        /// <param name="sort">是否对条目排序</param>
+        public void SetModel(SimpleTreeNode[] rootNodes, bool sort)
+        {
+            if (backend != null) backend.SetModel(rootNodes, sort);
+        }
+
+        /// <summary>
+        /// 批量更新内容节点
+        /// </summary>
+        /// <param name="tasks">更新内容节点的任务</param>
+        public void UpdateNodes(SimpleTreeNodeUpdateTask[] tasks)
+        {
+            if (backend != null) backend.UpdateNodes(tasks);
+        }
+
+        /// <summary>
+        /// 获取当前选中条目的键对象
+        /// </summary>
+        public object GetSelectedKey()
+        {
+            return backend == null ? null : backend.GetSelectedKey();
+        }
+
+        /// <summary>
+        /// 选中条目切换事件
+        /// </summary>
+        public event EventHandler SelectedItemChanged;
+
+        /// <summary>
+        /// 选中条目双击或回车事件
+        /// </summary>
+        public event EventHandler SelectedItemActivated;
+
+        public void OnSelectedItemChanged()
+        {
+            if (SelectedItemChanged != null) SelectedItemChanged(this, null);
+        }
+
+        public void OnSelectedItemActivated()
+        {
+            if (SelectedItemActivated != null) SelectedItemActivated(this, null);
+        }
+
+        public static SimpleTreeViewFactory Factory { private get; set; }
+
+		private SimpleTreeViewBackend backend;
+    }
+
+    /// <summary>
+    /// (api:eto=2.10.0) 简易树状视图的内容节点
+    /// </summary>
+    public class SimpleTreeNode
+    {
+        /// <summary>
+        /// 条目的键对象
+        /// </summary>
+        public object Key { get; set; }
+
+        /// <summary>
+        /// 显示文本内容
+        /// </summary>
+        public String Text { get; set; }
+
+        /// <summary>
+        /// 文本颜色，Colors.Transparent表示使用默认颜色
+        /// </summary>
+        public Color TextColor { get; set; }
+
+        /// <summary>
+        /// 条目背景颜色，Colors.Transparent表示使用默认颜色
+        /// </summary>
+        public Color BackgroundColor { get; set; }
+
+        /// <summary>
+        /// 子节点集合
+        /// </summary>
+        public List<SimpleTreeNode> ChildNodes { get { return childNodes; } }
+
+        /// <summary>
+        /// 子节点是否展开显示
+        /// </summary>
+        public bool ChildNodesExpanded { get; set; }
+
+        private List<SimpleTreeNode> childNodes = new List<SimpleTreeNode>();
+    }
+
+    /// <summary>
+    /// (api:eto=2.10.0) 更新简易树状视图内容节点的任务
+    /// </summary>
+    public class SimpleTreeNodeUpdateTask
+    {
+        /// <summary>
+        /// 条目的键对象
+        /// </summary>
+        public object Key { get; set; }
+
+        /// <summary>
+        /// 文本颜色，null表示不更新，Colors.Transparent表示设置为默认颜色
+        /// </summary>
+        public Color? TextColor { get; set; }
+
+        /// <summary>
+        /// 条目背景颜色，null表示不更新，Colors.Transparent表示设置为默认颜色
+        /// </summary>
+        public Color? BackgroundColor { get; set; }
+    }
+
+	public interface SimpleTreeViewCallback
+	{
+        void OnSelectedItemChanged();
+        void OnSelectedItemActivated();
+	}
+
+	public interface SimpleTreeViewBackend
+	{
+        void SetModel(SimpleTreeNode[] rootNodes, bool sort);
+        void UpdateNodes(SimpleTreeNodeUpdateTask[] tasks);
+        object GetSelectedKey();
+	}
+
+	public interface SimpleTreeViewFactory
+	{
+		void CreateSimpleTreeViewBackend(SimpleTreeViewCallback callback, out Control etoControl, out SimpleTreeViewBackend backend);
+	}
+
+    public class DefaultSimpleTreeViewFactory : SimpleTreeViewFactory
+    {
+        public void CreateSimpleTreeViewBackend(SimpleTreeViewCallback callback, out Control etoControl, out SimpleTreeViewBackend backend)
+        {
+            var control = new DefaultSimpleTreeViewBackend(callback);
+            etoControl = control;
+            backend = control;
+        }
+    }
+
+    class DefaultSimpleTreeViewBackend : TreeGridView, SimpleTreeViewBackend
+    {
+        public DefaultSimpleTreeViewBackend(SimpleTreeViewCallback callback)
+        {
+            this.callback = callback;
+
+            ShowHeader = false;
+
+            column = new GridColumn{ DataCell = new TextBoxCell(0), Width = this.Sizer(100) };
+            Columns.Add(column);
+
+            SelectedItemChanged += delegate
+            {
+                callback.OnSelectedItemChanged();
+            };
+
+            Activated += delegate
+            {
+                callback.OnSelectedItemActivated();
+            };
+
+            CellFormatting += (o, e) =>
+            {
+                var node = (e.Item as TreeGridItem).Tag as SimpleTreeNode;
+                if (node.TextColor != Colors.Transparent) e.ForegroundColor = node.TextColor;
+                else e.ForegroundColor = SystemColors.ControlText;
+                if (node.BackgroundColor != Colors.Transparent) e.BackgroundColor = node.BackgroundColor;
+                else e.BackgroundColor = SystemColors.ControlBackground;
+            };
+
+            SizeChanged += delegate
+            {
+                var width = this.GetLogicalWidth();
+                if (width > 30)
+                {
+                    var targetWidth = this.Sizer(width - 20);
+                    if (column.Width != targetWidth)
+                    {
+                        if (setWidthTimer != null)
+                        {
+                            setWidthTimer.Stop();
+                            setWidthTimer = null;
+                        }
+                        setWidthTimer = new UITimer();
+                        setWidthTimer.Interval = 0.05;
+                        setWidthTimer.Elapsed += delegate
+                        {
+                            setWidthTimer.Stop();
+                            setWidthTimer = null;
+                            column.Width = targetWidth;
+                        };
+                        setWidthTimer.Start();
+                    }
+                }
+            };
+        }
+
+        public void SetModel(SimpleTreeNode[] rootNodes, bool sort)
+        {
+            DataStore = null;
+            nodeMap.Clear();
+
+            var model = new TreeGridItemCollection();
+            if(rootNodes != null)
+            {
+                foreach (var node in rootNodes)
+                {
+                    if (node.Key == null || nodeMap.ContainsKey(node.Key)) continue;
+                    if (String.IsNullOrEmpty(node.Text)) continue;
+
+                    var rootItem = new TreeGridItem{ Tag = node, Values = new String[] { node.Text } };
+                    nodeMap[node.Key] = node;
+                    if (node.ChildNodes != null && node.ChildNodes.Count > 0)
+                    {
+                        addChildNodes(rootItem, node.ChildNodes, sort);
+                        if (rootItem.Children.Count > 0)
+                        {
+                            if (node.ChildNodesExpanded) rootItem.Expanded = true;
+                            if (sort) rootItem.Children.Sort(sortFunc);
+                        }
+                    }
+                    model.Add(rootItem);
+                }
+            }
+
+            DataStore = model;
+        }
+
+        public void UpdateNodes(SimpleTreeNodeUpdateTask[] tasks)
+        {
+            if (tasks.Length == 0) return;
+
+            bool updated = false;
+            foreach (var task in tasks)
+            {
+                if (task.Key == null || !nodeMap.ContainsKey(task.Key)) continue;
+
+                var node = nodeMap[task.Key];
+                if (task.TextColor != null && task.TextColor != node.TextColor)
+                {
+                    node.TextColor = task.TextColor.Value;
+                    updated = true;
+                }
+                if (task.BackgroundColor != null && task.BackgroundColor != node.BackgroundColor)
+                {
+                    node.BackgroundColor = task.BackgroundColor.Value;
+                    updated = true;
+                }
+            }
+
+            if (updated) ReloadData();
+        }
+
+        public object GetSelectedKey()
+        {
+            if (SelectedItem == null) return null;
+            else return ((SelectedItem as TreeGridItem).Tag as SimpleTreeNode).Key;
+        }
+
+        private void addChildNodes(TreeGridItem parentItem, List<SimpleTreeNode> childNodes, bool sort)
+        {
+            foreach (var node in childNodes)
+            {
+                if (nodeMap.ContainsKey(node.Key)) continue;
+                if (String.IsNullOrEmpty(node.Text)) continue;
+
+                var childItem = new TreeGridItem{ Tag = node, Values = new String[] { node.Text } };
+                nodeMap[node.Key] = node;
+                if (node.ChildNodes != null && node.ChildNodes.Count > 0)
+                {
+                    addChildNodes(childItem, node.ChildNodes, sort);
+                    if (childItem.Children.Count > 0)
+                    {
+                        if (node.ChildNodesExpanded) childItem.Expanded = true;
+                        if (sort) childItem.Children.Sort(sortFunc);
+                    }
+                }
+                parentItem.Children.Add(childItem);
+            }
+        }
+
+        private int sortFunc(ITreeGridItem x, ITreeGridItem y)
+        {
+            var xs = (x as TreeGridItem).GetValue(0) as String;
+            var ys = (y as TreeGridItem).GetValue(0) as String;
+            return xs.CompareTo(ys);
+        }
+
+        private SimpleTreeViewCallback callback;
+        private GridColumn column;
+        private Dictionary<object, SimpleTreeNode> nodeMap = new Dictionary<object, SimpleTreeNode>();
+        private UITimer setWidthTimer;
+    }
+}
--- ASEvaAPIEtoTest/BasicPageB.cs
+++ ASEvaAPIEtoTest/BasicPageB.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ASEva.Utility;
 using ASEva.Samples;
 using ASEva.UIEto;
@@ -22,8 +23,8 @@ namespace ASEvaAPIEtoTest
             var layoutLists = layoutPanel1.AddColumnLayout(false, 120, 0, 8);
             initBasicTabPageBLists(layoutLists);
 
-            var treeView = layoutPanel1.AddControl(new TreeGridView(), true) as TreeGridView;
-            initBasicTabPageBTreeView(treeView);
+            var treeViewLayout = layoutPanel1.AddColumnLayout(true, 2);
+            initBasicTabPageBTreeView(treeViewLayout);
 
             var layoutFlowItems = panel2.SetContentAsColumnLayout();
             initBasicTabPageBFlowItems(layoutFlowItems);
@@ -32,47 +33,58 @@ namespace ASEvaAPIEtoTest
         private void initBasicTabPageBLists(StackLayout layout)
         {
             var listBox = layout.AddControl(new ListBox(), true) as ListBox;
-            for (int i = 0; i < 26; i++) listBox.Items.Add(t.Format("basic-list-item", (char)('A' + i)));
+            for (int i = 1; i <= 1000; i++) listBox.Items.Add(t.Format("basic-list-item", i.ToString()));
 
             var checkListBox = layout.AddControl(new CheckableListBox(), true) as CheckableListBox;
-            for (int i = 0; i < 26; i++) checkListBox.AddItem(t.Format("basic-list-item-short", (char)('A' + i)), i % 2 == 0, (i / 2) % 2 == 0);
+            for (int i = 1; i <= 1000; i++) checkListBox.AddItem(t.Format("basic-list-item-short", i.ToString()), i % 2 == 0, (i / 2) % 2 == 0);
         }
 
-        private void initBasicTabPageBTreeView(TreeGridView view)
+        private void initBasicTabPageBTreeView(StackLayout layout)
         {
-            view.ShowHeader  = false;
-            view.Columns.Add(new GridColumn{ DataCell = new TextBoxCell(0), Width = this.Sizer(150) });
+            var view = layout.AddControl(new SimpleTreeView(), true) as SimpleTreeView;
+            var button = layout.AddLinkButton(t["basic-grid-change-color"]);
 
-            var model = new TreeGridItemCollection();
-
-            var parent1 = new TreeGridItem{ Tag = "p1", Values = new String[] { t.Format("basic-tree-parent", 1) } };
-            var child1 = new TreeGridItem{ Tag = "c1", Values = new String[] { t.Format("basic-tree-child", 1) } };
-            parent1.Children.Add(child1);
-            var child2 = new TreeGridItem{ Tag = "c2", Values = new String[] { t.Format("basic-tree-child", 2) } };
-            parent1.Children.Add(child2);
-            model.Add(parent1);
-
-            var parent2 = new TreeGridItem{ Tag = "p2", Values = new String[] { t.Format("basic-tree-parent", 2) } };
-            var child3 = new TreeGridItem{ Tag = "c3", Values = new String[] { t.Format("basic-tree-child", 3) } };
-            parent2.Children.Add(child3);
-            var child4 = new TreeGridItem{ Tag = "c4", Values = new String[] { t.Format("basic-tree-child", 4) } };
-            parent2.Children.Add(child4);
-            model.Add(parent2);
-
-            view.DataStore = model;
-
-            view.CellFormatting += (sender, args) =>
+            var parentNodes = new List<SimpleTreeNode>();
+            for (int i = 1; i <= 99; i++)
             {
-                args.ForegroundColor = (args.Item as TreeGridItem).Tag as String switch
+                var parentNode = new SimpleTreeNode();
+                parentNode.Key = i.ToString();
+                parentNode.Text = t.Format("basic-tree-parent", i.ToString("D2"));
+                if (i % 2 == 0) parentNode.BackgroundColor = Colors.LightGrey;
+                if (i > 90) parentNode.ChildNodesExpanded = true;
+                for (int j = 1; j <= 99; j++)
                 {
-                    "c2" => Colors.Red,
-                    "p2" => Colors.Blue,
-                    _ => Colors.Black
-                };
-            };
-            view.Activated += delegate
+                    var childNode = new SimpleTreeNode();
+                    childNode.Key = i + "." + j;
+                    childNode.Text = t.Format("basic-tree-child", j.ToString("D2"));
+                    if (j % 2 == 0) childNode.TextColor = Colors.Blue;
+                    parentNode.ChildNodes.Add(childNode);
+                }
+                parentNodes.Add(parentNode);
+            }
+            view.SetModel(parentNodes.ToArray(), true);
+
+            view.SelectedItemActivated += delegate
             {
-                if (view.SelectedItem != null) MessageBox.Show((view.SelectedItem as TreeGridItem).Values[0] as String, "");
+                var selectedKey = view.GetSelectedKey();
+                if (selectedKey != null) MessageBox.Show(selectedKey as String, "");
+            };
+
+            button.Click += delegate
+            {
+                var tasks = new List<SimpleTreeNodeUpdateTask>();
+                for (int i = 1; i <= 99; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        var task = new SimpleTreeNodeUpdateTask();
+                        task.Key = i.ToString();
+                        task.TextColor = Colors.White;
+                        task.BackgroundColor = Colors.DimGray;
+                        tasks.Add(task);
+                    }
+                }
+                view.UpdateNodes(tasks.ToArray());
             };
         }
 
