b7e08a5b2e625439f37c82faf6671de48751eedd
Ken <sdken777@gmail.com>
2023-04-03 11:14:52 +0800

    eto-2.10.5:新增APIInfo.GetEtoLibVersion

--- ASEvaAPIEto/APIInfo.cs
+++ ASEvaAPIEto/APIInfo.cs
@@ -1,17 +1,19 @@
 ﻿/*! \mainpage
  * 此类库为ASEva-API中基于Eto.Forms的部分。详见 ASEva.UIEto \n
- * 本文档对应API版本：2.10.4\n
+ * 本文档对应API版本：2.10.5\n
  * \n
  * Eto.Forms官方仓库: https://github.com/picoe/Eto \n
  * API文档: https://pages.picoe.ca/docs/api
  */
 
 using System;
+using System.IO;
+using System.Reflection;
 
 namespace ASEva.UIEto
 {
     /// <summary>
-    /// version=2.10.4
+    /// version=2.10.5
     /// </summary>
     public class APIInfo
     {
@@ -21,7 +23,25 @@ namespace ASEva.UIEto
         /// <returns>API版本</returns>
         public static Version GetAPIVersion()
         {
-            return new Version(2, 10, 4, 0); // StackLayout.AddSpace支持添加指定大小空间
+            return new Version(2, 10, 5, 0); // 新增APIInfo.GetEtoLibVersion
+        }
+
+        /// <summary>
+        /// (api:eto=2.10.5) 获取Eto库版本
+        /// </summary>
+        /// <returns>Eto库版本</returns>
+        public static Version GetEtoLibVersion()
+        {
+            var apiEtoPath = Assembly.GetExecutingAssembly().Location;
+            var etoLibPath = Path.GetDirectoryName(apiEtoPath) + Path.DirectorySeparatorChar + "Eto.dll";
+
+            try
+            {
+                var etoLib = Assembly.LoadFrom(etoLibPath);
+                var etoLibName = etoLib.GetName();
+                return etoLibName.Version;
+            }
+            catch (Exception) { return null;}
         }
     }
 }
