589a836c8c78ba6c47f5736f71f5ce4988022912
Ken <sdken777@gmail.com>
2023-09-25 15:30:51 +0800

    corewf-2.5.4:设置WinformIconExtensions

---
+++ ASEvaAPICoreWF/Extension/WinformIconExtensions.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using ASEva;
+using ASEva.Samples;
+using ASEva.UIEto;
+
+namespace ASEva.UICoreWF
+{
+    /// <summary>
+    /// (api:corewf=2.5.4) 方便生成图标的扩展
+    /// </summary>
+    public static class WinformIconExtensions
+    {
+        public static Icon ToWinformIcon(this byte[] imageData)
+        {
+            var etoIcon = imageData.ToIcon();
+            if (etoIcon == null) return null;
+            else return etoIcon.ControlObject as Icon;
+        }
+
+        public static Icon ToWinformIcon(this Bitmap bitmap)
+        {
+            var image = ImageConverter.ConvertFromBitmap(bitmap);
+            if (image == null) return null;
+            else return ToWinformIcon(image);
+        }
+
+        public static Icon ToWinformIcon(this CommonImage image)
+        {
+            var etoIcon = image.ToIcon();
+            if (etoIcon == null) return null;
+            else return etoIcon.ControlObject as Icon;
+        }
+    }
+}
