b9b848b1bd9a803580b958c9e3c83c915549540c
Ken <sdken777@gmail.com>
2024-01-09 13:36:57 +0800

    gtk-2.9.2.4:细节修正

--- ASEvaAPIGtk/APIInfo.cs
+++ ASEvaAPIGtk/APIInfo.cs
@@ -31,7 +31,7 @@ namespace ASEva.UIGtk
         /// <returns>API版本</returns>
         public static Version GetAPIVersion()
         {
-            return new Version(2, 9, 2, 3); // Update log / 更新记录: 优化CheckedListBox
+            return new Version(2, 9, 2, 4); // Update log / 更新记录: 细节修正
         }
     }
 }
