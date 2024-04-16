9a701835f8aa324f7417ab5937adcf64b0a071ff
Ken <sdken777@gmail.com>
2024-04-16 11:45:04 +0800

    3.0更新中：工程初始化完成

--- export.sh
+++ export.sh
@@ -41,10 +41,11 @@ if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
     cp -vf "$CUR_DIR"/bin64/ASEvaAPICoreWF.dll $TARGET_DIR/bin64/
     cp -vf "$CUR_DIR"/bin64/ASEvaAPIWpf.dll $TARGET_DIR/bin64/
     cp -vf "$CUR_DIR"/bin64/PortableSharpGL.dll $TARGET_DIR/bin64/
-    cp -vf "$CUR_DIR"/3party/eto/* $TARGET_DIR/bin64/
-    cp -vf "$CUR_DIR"/3party/corewf-wpf/* $TARGET_DIR/bin64/
+    cp -vf "$CUR_DIR"/3party/eto-common/* $TARGET_DIR/bin64/
+    cp -vf "$CUR_DIR"/3party/eto-corewf-wpf/* $TARGET_DIR/bin64/
+    cp -vf "$CUR_DIR"/3party/skia-common/* $TARGET_DIR/bin64/
     if [ "$EXPORT_SKIA_NATIVE" = "y" ]; then
-        cp -vf "$CUR_DIR"/3party/windows-skia-native/* $TARGET_DIR/bin64/
+        cp -vf "$CUR_DIR"/3party/skia-windows-x64/* $TARGET_DIR/bin64/
     fi
 fi
 
@@ -55,10 +56,11 @@ if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
     cp -vf "$CUR_DIR"/binx/ASEvaAPIEto.dll $TARGET_DIR/binx/
     cp -vf "$CUR_DIR"/binx/ASEvaAPIGtk.dll $TARGET_DIR/binx/
     cp -vf "$CUR_DIR"/binx/PortableSharpGL.dll $TARGET_DIR/binx/
-    cp -vf "$CUR_DIR"/3party/eto/* $TARGET_DIR/binx/
-    cp -vf "$CUR_DIR"/3party/gtk/* $TARGET_DIR/binx/
+    cp -vf "$CUR_DIR"/3party/eto-common/* $TARGET_DIR/binx/
+    cp -vf "$CUR_DIR"/3party/eto-gtk/* $TARGET_DIR/binx/
+    cp -vf "$CUR_DIR"/3party/skia-common/* $TARGET_DIR/binx/
     if [ "$EXPORT_SKIA_NATIVE" = "y" ]; then
-        cp -vf "$CUR_DIR"/3party/linux-x64-skia-native/* $TARGET_DIR/binx/
+        cp -vf "$CUR_DIR"/3party/skia-linux-x64/* $TARGET_DIR/binx/
     fi
 fi
 
@@ -69,25 +71,26 @@ if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
     cp -vf "$CUR_DIR"/binxa/ASEvaAPIEto.dll $TARGET_DIR/binxa/
     cp -vf "$CUR_DIR"/binxa/ASEvaAPIGtk.dll $TARGET_DIR/binxa/
     cp -vf "$CUR_DIR"/binxa/PortableSharpGL.dll $TARGET_DIR/binxa/
-    cp -vf "$CUR_DIR"/3party/eto/* $TARGET_DIR/binxa/
-    cp -vf "$CUR_DIR"/3party/gtk/* $TARGET_DIR/binxa/
+    cp -vf "$CUR_DIR"/3party/eto-common/* $TARGET_DIR/binxa/
+    cp -vf "$CUR_DIR"/3party/eto-gtk/* $TARGET_DIR/binxa/
+    cp -vf "$CUR_DIR"/3party/skia-common/* $TARGET_DIR/binxa/
     if [ "$EXPORT_SKIA_NATIVE" = "y" ]; then
-        cp -vf "$CUR_DIR"/3party/linux-arm64-skia-native/* $TARGET_DIR/binxa/
+        cp -vf "$CUR_DIR"/3party/skia-linux-arm64/* $TARGET_DIR/binxa/
     fi
 fi
 
-mkdir -vp $TARGET_DIR/binm
-cp -vf "$CUR_DIR"/binm/ASEvaAPI.dll $TARGET_DIR/binm/
-cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binm/
+mkdir -vp $TARGET_DIR/binma
+cp -vf "$CUR_DIR"/binma/ASEvaAPI.dll $TARGET_DIR/binma/
+cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binma/
 if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
-    cp -vf "$CUR_DIR"/binm/ASEvaAPIEto.dll $TARGET_DIR/binm/
-    cp -vf "$CUR_DIR"/binm/ASEvaAPIMonoMac.dll $TARGET_DIR/binm/
-    cp -vf "$CUR_DIR"/binm/PortableSharpGL.dll $TARGET_DIR/binm/
-    cp -vf "$CUR_DIR"/3party/eto/* $TARGET_DIR/binm/
-    cp -vf "$CUR_DIR"/3party/monomac/* $TARGET_DIR/binm/
-    cp -vf "$CUR_DIR"/3party/macos/* $TARGET_DIR/binm/
+    cp -vf "$CUR_DIR"/binma/ASEvaAPIEto.dll $TARGET_DIR/binma/
+    cp -vf "$CUR_DIR"/binma/ASEvaAPIMonoMac.dll $TARGET_DIR/binma/
+    cp -vf "$CUR_DIR"/binma/PortableSharpGL.dll $TARGET_DIR/binma/
+    cp -vf "$CUR_DIR"/3party/eto-common/* $TARGET_DIR/binma/
+    cp -vf "$CUR_DIR"/3party/eto-monomac/* $TARGET_DIR/binma/
+    cp -vf "$CUR_DIR"/3party/skia-common/* $TARGET_DIR/binma/
     if [ "$EXPORT_SKIA_NATIVE" = "y" ]; then
-        cp -vf "$CUR_DIR"/3party/macos-skia-native/* $TARGET_DIR/binm/
+        cp -vf "$CUR_DIR"/3party/skia-macos-any/* $TARGET_DIR/binma/
     fi
 fi
 
@@ -96,6 +99,7 @@ if [ "$EXPORT_DEVELOPER" = "y" ]; then
     cp -vf "$CUR_DIR"/bin64/ASEvaAPI.xml $TARGET_DIR/bin64/
     if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/3party/eto-doc/* $TARGET_DIR/bin64/
+        cp -vf "$CUR_DIR"/3party/skia-doc/* $TARGET_DIR/bin64/
         cp -vf "$CUR_DIR"/bin64/ASEvaAPIEto.xml $TARGET_DIR/bin64/
         cp -vf "$CUR_DIR"/bin64/ASEvaAPICoreWF.xml $TARGET_DIR/bin64/
         cp -vf "$CUR_DIR"/bin64/ASEvaAPIWpf.xml $TARGET_DIR/bin64/
@@ -105,6 +109,7 @@ if [ "$EXPORT_DEVELOPER" = "y" ]; then
     cp -vf "$CUR_DIR"/binx/ASEvaAPI.xml $TARGET_DIR/binx/
     if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/3party/eto-doc/* $TARGET_DIR/binx/
+        cp -vf "$CUR_DIR"/3party/skia-doc/* $TARGET_DIR/binx/
         cp -vf "$CUR_DIR"/binx/ASEvaAPIEto.xml $TARGET_DIR/binx/
         cp -vf "$CUR_DIR"/binx/ASEvaAPIGtk.xml $TARGET_DIR/binx/
     fi
@@ -113,16 +118,18 @@ if [ "$EXPORT_DEVELOPER" = "y" ]; then
     cp -vf "$CUR_DIR"/binxa/ASEvaAPI.xml $TARGET_DIR/binxa/
     if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/3party/eto-doc/* $TARGET_DIR/binxa/
+        cp -vf "$CUR_DIR"/3party/skia-doc/* $TARGET_DIR/binxa/
         cp -vf "$CUR_DIR"/binxa/ASEvaAPIEto.xml $TARGET_DIR/binxa/
         cp -vf "$CUR_DIR"/binxa/ASEvaAPIGtk.xml $TARGET_DIR/binxa/
     fi
 
-    mkdir -vp $TARGET_DIR/binm/debug
-    cp -vf "$CUR_DIR"/binm/ASEvaAPI.xml $TARGET_DIR/binm/
+    mkdir -vp $TARGET_DIR/binma/debug
+    cp -vf "$CUR_DIR"/binma/ASEvaAPI.xml $TARGET_DIR/binma/
     if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
-        cp -vf "$CUR_DIR"/3party/eto-doc/* $TARGET_DIR/binm/
-        cp -vf "$CUR_DIR"/binm/ASEvaAPIEto.xml $TARGET_DIR/binm/
-        cp -vf "$CUR_DIR"/binm/ASEvaAPIMonoMac.xml $TARGET_DIR/binm/
+        cp -vf "$CUR_DIR"/3party/eto-doc/* $TARGET_DIR/binma/
+        cp -vf "$CUR_DIR"/3party/skia-doc/* $TARGET_DIR/binma/
+        cp -vf "$CUR_DIR"/binma/ASEvaAPIEto.xml $TARGET_DIR/binma/
+        cp -vf "$CUR_DIR"/binma/ASEvaAPIMonoMac.xml $TARGET_DIR/binma/
     fi
 fi
 
@@ -135,10 +142,11 @@ if [ "$EXPORT_RUNTIME_DEBUG" = "y" ]; then
         cp -vf "$CUR_DIR"/bin64/ASEvaAPICoreWF.dll $TARGET_DIR/bin64/debug/
         cp -vf "$CUR_DIR"/bin64/ASEvaAPIWpf.dll $TARGET_DIR/bin64/debug/
         cp -vf "$CUR_DIR"/bin64/PortableSharpGL.dll $TARGET_DIR/bin64/debug/
-        cp -vf "$CUR_DIR"/3party/eto/* $TARGET_DIR/bin64/debug/
-        cp -vf "$CUR_DIR"/3party/corewf-wpf/* $TARGET_DIR/bin64/debug/
+        cp -vf "$CUR_DIR"/3party/eto-common/* $TARGET_DIR/bin64/debug/
+        cp -vf "$CUR_DIR"/3party/eto-corewf-wpf/* $TARGET_DIR/bin64/debug/
+        cp -vf "$CUR_DIR"/3party/skia-common/* $TARGET_DIR/bin64/debug/
         if [ "$EXPORT_SKIA_NATIVE" = "y" ]; then
-            cp -vf "$CUR_DIR"/3party/windows-skia-native/* $TARGET_DIR/bin64/debug/
+            cp -vf "$CUR_DIR"/3party/skia-windows-x64/* $TARGET_DIR/bin64/debug/
         fi
     fi
 
@@ -149,10 +157,11 @@ if [ "$EXPORT_RUNTIME_DEBUG" = "y" ]; then
         cp -vf "$CUR_DIR"/binx/ASEvaAPIEto.dll $TARGET_DIR/binx/debug/
         cp -vf "$CUR_DIR"/binx/ASEvaAPIGtk.dll $TARGET_DIR/binx/debug/
         cp -vf "$CUR_DIR"/binx/PortableSharpGL.dll $TARGET_DIR/binx/debug/
-        cp -vf "$CUR_DIR"/3party/eto/* $TARGET_DIR/binx/debug/
-        cp -vf "$CUR_DIR"/3party/gtk/* $TARGET_DIR/binx/debug/
+        cp -vf "$CUR_DIR"/3party/eto-common/* $TARGET_DIR/binx/debug/
+        cp -vf "$CUR_DIR"/3party/eto-gtk/* $TARGET_DIR/binx/debug/
+        cp -vf "$CUR_DIR"/3party/skia-common/* $TARGET_DIR/binx/debug/
         if [ "$EXPORT_SKIA_NATIVE" = "y" ]; then
-            cp -vf "$CUR_DIR"/3party/linux-x64-skia-native/* $TARGET_DIR/binx/debug/
+            cp -vf "$CUR_DIR"/3party/skia-linux-x64/* $TARGET_DIR/binx/debug/
         fi
     fi
 
@@ -163,25 +172,26 @@ if [ "$EXPORT_RUNTIME_DEBUG" = "y" ]; then
         cp -vf "$CUR_DIR"/binxa/ASEvaAPIEto.dll $TARGET_DIR/binxa/debug/
         cp -vf "$CUR_DIR"/binxa/ASEvaAPIGtk.dll $TARGET_DIR/binxa/debug/
         cp -vf "$CUR_DIR"/binxa/PortableSharpGL.dll $TARGET_DIR/binxa/debug/
-        cp -vf "$CUR_DIR"/3party/eto/* $TARGET_DIR/binxa/debug/
-        cp -vf "$CUR_DIR"/3party/gtk/* $TARGET_DIR/binxa/debug/
+        cp -vf "$CUR_DIR"/3party/eto-common/* $TARGET_DIR/binxa/debug/
+        cp -vf "$CUR_DIR"/3party/eto-gtk/* $TARGET_DIR/binxa/debug/
+        cp -vf "$CUR_DIR"/3party/skia-common/* $TARGET_DIR/binxa/debug/
         if [ "$EXPORT_SKIA_NATIVE" = "y" ]; then
-            cp -vf "$CUR_DIR"/3party/linux-arm64-skia-native/* $TARGET_DIR/binxa/debug/
+            cp -vf "$CUR_DIR"/3party/skia-linux-arm64/* $TARGET_DIR/binxa/debug/
         fi
     fi
 
-    mkdir -vp $TARGET_DIR/binm/debug
-    cp -vf "$CUR_DIR"/binm/ASEvaAPI.dll $TARGET_DIR/binm/debug/
-    cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binm/debug/
+    mkdir -vp $TARGET_DIR/binma/debug
+    cp -vf "$CUR_DIR"/binma/ASEvaAPI.dll $TARGET_DIR/binma/debug/
+    cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binma/debug/
     if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
-        cp -vf "$CUR_DIR"/binm/ASEvaAPIEto.dll $TARGET_DIR/binm/debug/
-        cp -vf "$CUR_DIR"/binm/ASEvaAPIMonoMac.dll $TARGET_DIR/binm/debug/
-        cp -vf "$CUR_DIR"/binm/PortableSharpGL.dll $TARGET_DIR/binm/debug/
-        cp -vf "$CUR_DIR"/3party/eto/* $TARGET_DIR/binm/debug/
-        cp -vf "$CUR_DIR"/3party/monomac/* $TARGET_DIR/binm/debug/
-        cp -vf "$CUR_DIR"/3party/macos/* $TARGET_DIR/binm/debug/
+        cp -vf "$CUR_DIR"/binma/ASEvaAPIEto.dll $TARGET_DIR/binma/debug/
+        cp -vf "$CUR_DIR"/binma/ASEvaAPIMonoMac.dll $TARGET_DIR/binma/debug/
+        cp -vf "$CUR_DIR"/binma/PortableSharpGL.dll $TARGET_DIR/binma/debug/
+        cp -vf "$CUR_DIR"/3party/eto-common/* $TARGET_DIR/binma/debug/
+        cp -vf "$CUR_DIR"/3party/eto-monomac/* $TARGET_DIR/binma/debug/
+        cp -vf "$CUR_DIR"/3party/skia-common/* $TARGET_DIR/binma/debug/
         if [ "$EXPORT_SKIA_NATIVE" = "y" ]; then
-            cp -vf "$CUR_DIR"/3party/macos-skia-native/* $TARGET_DIR/binm/debug/
+            cp -vf "$CUR_DIR"/3party/skia-macos-any/* $TARGET_DIR/binma/debug/
         fi
     fi
 fi
--- gen_apidoc.sh
+++ gen_apidoc.sh
@@ -1,39 +1,39 @@
 CUR_DIR=$(cd $(dirname $0) && pwd)
 
-mkdir -vp ~/Desktop/D01006_ASEvaAPIDoc_Chinese
-mkdir -vp ~/Desktop/D01006_ASEvaAPIDoc_English
+mkdir -vp ~/Desktop/D01010_ASEvaAPIDoc_Chinese
+mkdir -vp ~/Desktop/D01010_ASEvaAPIDoc_English
 
 cd "$CUR_DIR"/doc
 
 DOCUMENT_LANGUAGE=Chinese doxygen Doxyfile-ASEvaAPI
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_Chinese/Common/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_Chinese/Common/
 DOCUMENT_LANGUAGE=English doxygen Doxyfile-ASEvaAPI
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_English/Common/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_English/Common/
 
 DOCUMENT_LANGUAGE=Chinese doxygen Doxyfile-ASEvaAPIEto
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_Chinese/Eto/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_Chinese/Eto/
 DOCUMENT_LANGUAGE=English doxygen Doxyfile-ASEvaAPIEto
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_English/Eto/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_English/Eto/
 
 DOCUMENT_LANGUAGE=Chinese doxygen Doxyfile-ASEvaAPICoreWF
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_Chinese/CoreWF/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_Chinese/CoreWF/
 DOCUMENT_LANGUAGE=English doxygen Doxyfile-ASEvaAPICoreWF
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_English/CoreWF/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_English/CoreWF/
 
 DOCUMENT_LANGUAGE=Chinese doxygen Doxyfile-ASEvaAPIWpf
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_Chinese/Wpf/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_Chinese/Wpf/
 DOCUMENT_LANGUAGE=English doxygen Doxyfile-ASEvaAPIWpf
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_English/Wpf/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_English/Wpf/
 
 DOCUMENT_LANGUAGE=Chinese doxygen Doxyfile-ASEvaAPIGtk
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_Chinese/Gtk/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_Chinese/Gtk/
 DOCUMENT_LANGUAGE=English doxygen Doxyfile-ASEvaAPIGtk
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_English/Gtk/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_English/Gtk/
 
 DOCUMENT_LANGUAGE=Chinese doxygen Doxyfile-ASEvaAPIMonoMac
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_Chinese/MonoMac/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_Chinese/MonoMac/
 DOCUMENT_LANGUAGE=English doxygen Doxyfile-ASEvaAPIMonoMac
-cp -vf ./index.html ~/Desktop/D01006_ASEvaAPIDoc_English/MonoMac/
+cp -vf ./index.html ~/Desktop/D01010_ASEvaAPIDoc_English/MonoMac/
 
 echo "Done. (ASEvaAPI/gen_apidoc.sh)"
 sleep 1
