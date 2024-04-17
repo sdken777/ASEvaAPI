2786bb0d43bd49a0d0619f0f527eb384866a95c3
Ken <sdken777@gmail.com>
2024-04-17 15:13:38 +0800

    更新export.sh

--- export.sh
+++ export.sh
@@ -1,11 +1,11 @@
-TARGET_NAME=EtoSDK-Full
+TARGET_NAME=ApiGuiSDK-Full
 
 CUR_DIR=$(cd $(dirname $0) && pwd)
 CUR_DATE=`date +%Y%m%d`
 
 EXPORT_DEVELOPER=y
 EXPORT_RUNTIME_DEBUG=y
-EXPORT_UI_LIBRARY=y
+EXPORT_GUI_LIBRARY=y
 EXPORT_SKIA_NATIVE=n
 if [ "$1" = "" ]; then
     TARGET_DIR=~/Desktop/$CUR_DATE-$TARGET_NAME
@@ -21,12 +21,12 @@ else
     fi
     if [ "$2" = "runtime-with-debug-without-ui" ]; then
         EXPORT_DEVELOPER=n
-        EXPORT_UI_LIBRARY=n
+        EXPORT_GUI_LIBRARY=n
     fi
     if [ "$2" = "runtime-without-debug-without-ui" ]; then
         EXPORT_DEVELOPER=n
         EXPORT_RUNTIME_DEBUG=n
-        EXPORT_UI_LIBRARY=n
+        EXPORT_GUI_LIBRARY=n
     fi
     if [ "$3" = "with-skia-native" ]; then
         EXPORT_SKIA_NATIVE=y
@@ -36,7 +36,7 @@ fi
 mkdir -vp $TARGET_DIR/bin64
 cp -vf "$CUR_DIR"/bin64/ASEvaAPI.dll $TARGET_DIR/bin64/
 cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/bin64/
-if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
     cp -vf "$CUR_DIR"/bin64/ASEvaAPIEto.dll $TARGET_DIR/bin64/
     cp -vf "$CUR_DIR"/bin64/ASEvaAPICoreWF.dll $TARGET_DIR/bin64/
     cp -vf "$CUR_DIR"/bin64/ASEvaAPIWpf.dll $TARGET_DIR/bin64/
@@ -51,7 +51,7 @@ fi
 mkdir -vp $TARGET_DIR/binx
 cp -vf "$CUR_DIR"/binx/ASEvaAPI.dll $TARGET_DIR/binx/
 cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binx/
-if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
     cp -vf "$CUR_DIR"/binx/ASEvaAPIEto.dll $TARGET_DIR/binx/
     cp -vf "$CUR_DIR"/binx/ASEvaAPIGtk.dll $TARGET_DIR/binx/
     cp -vf "$CUR_DIR"/binx/PortableSharpGL.dll $TARGET_DIR/binx/
@@ -65,7 +65,7 @@ fi
 mkdir -vp $TARGET_DIR/binxa
 cp -vf "$CUR_DIR"/binxa/ASEvaAPI.dll $TARGET_DIR/binxa/
 cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binxa/
-if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
     cp -vf "$CUR_DIR"/binxa/ASEvaAPIEto.dll $TARGET_DIR/binxa/
     cp -vf "$CUR_DIR"/binxa/ASEvaAPIGtk.dll $TARGET_DIR/binxa/
     cp -vf "$CUR_DIR"/binxa/PortableSharpGL.dll $TARGET_DIR/binxa/
@@ -79,7 +79,7 @@ fi
 mkdir -vp $TARGET_DIR/binm
 cp -vf "$CUR_DIR"/binm/ASEvaAPI.dll $TARGET_DIR/binm/
 cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binm/
-if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
     cp -vf "$CUR_DIR"/binm/ASEvaAPIEto.dll $TARGET_DIR/binm/
     cp -vf "$CUR_DIR"/binm/ASEvaAPIMonoMac.dll $TARGET_DIR/binm/
     cp -vf "$CUR_DIR"/binm/PortableSharpGL.dll $TARGET_DIR/binm/
@@ -94,7 +94,7 @@ fi
 if [ "$EXPORT_DEVELOPER" = "y" ]; then
     mkdir -vp $TARGET_DIR/bin64/debug
     cp -vf "$CUR_DIR"/bin64/ASEvaAPI.xml $TARGET_DIR/bin64/
-    if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+    if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/3party/eto-doc/* $TARGET_DIR/bin64/
         cp -vf "$CUR_DIR"/bin64/ASEvaAPIEto.xml $TARGET_DIR/bin64/
         cp -vf "$CUR_DIR"/bin64/ASEvaAPICoreWF.xml $TARGET_DIR/bin64/
@@ -103,7 +103,7 @@ if [ "$EXPORT_DEVELOPER" = "y" ]; then
 
     mkdir -vp $TARGET_DIR/binx/debug
     cp -vf "$CUR_DIR"/binx/ASEvaAPI.xml $TARGET_DIR/binx/
-    if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+    if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/3party/eto-doc/* $TARGET_DIR/binx/
         cp -vf "$CUR_DIR"/binx/ASEvaAPIEto.xml $TARGET_DIR/binx/
         cp -vf "$CUR_DIR"/binx/ASEvaAPIGtk.xml $TARGET_DIR/binx/
@@ -111,7 +111,7 @@ if [ "$EXPORT_DEVELOPER" = "y" ]; then
 
     mkdir -vp $TARGET_DIR/binxa/debug
     cp -vf "$CUR_DIR"/binxa/ASEvaAPI.xml $TARGET_DIR/binxa/
-    if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+    if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/3party/eto-doc/* $TARGET_DIR/binxa/
         cp -vf "$CUR_DIR"/binxa/ASEvaAPIEto.xml $TARGET_DIR/binxa/
         cp -vf "$CUR_DIR"/binxa/ASEvaAPIGtk.xml $TARGET_DIR/binxa/
@@ -119,7 +119,7 @@ if [ "$EXPORT_DEVELOPER" = "y" ]; then
 
     mkdir -vp $TARGET_DIR/binm/debug
     cp -vf "$CUR_DIR"/binm/ASEvaAPI.xml $TARGET_DIR/binm/
-    if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+    if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/3party/eto-doc/* $TARGET_DIR/binm/
         cp -vf "$CUR_DIR"/binm/ASEvaAPIEto.xml $TARGET_DIR/binm/
         cp -vf "$CUR_DIR"/binm/ASEvaAPIMonoMac.xml $TARGET_DIR/binm/
@@ -130,7 +130,7 @@ if [ "$EXPORT_RUNTIME_DEBUG" = "y" ]; then
     mkdir -vp $TARGET_DIR/bin64/debug
     cp -vf "$CUR_DIR"/bin64/ASEvaAPI.dll $TARGET_DIR/bin64/debug/
     cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/bin64/debug/
-    if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+    if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/bin64/ASEvaAPIEto.dll $TARGET_DIR/bin64/debug/
         cp -vf "$CUR_DIR"/bin64/ASEvaAPICoreWF.dll $TARGET_DIR/bin64/debug/
         cp -vf "$CUR_DIR"/bin64/ASEvaAPIWpf.dll $TARGET_DIR/bin64/debug/
@@ -145,7 +145,7 @@ if [ "$EXPORT_RUNTIME_DEBUG" = "y" ]; then
     mkdir -vp $TARGET_DIR/binx/debug
     cp -vf "$CUR_DIR"/binx/ASEvaAPI.dll $TARGET_DIR/binx/debug/
     cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binx/debug/
-    if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+    if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/binx/ASEvaAPIEto.dll $TARGET_DIR/binx/debug/
         cp -vf "$CUR_DIR"/binx/ASEvaAPIGtk.dll $TARGET_DIR/binx/debug/
         cp -vf "$CUR_DIR"/binx/PortableSharpGL.dll $TARGET_DIR/binx/debug/
@@ -159,7 +159,7 @@ if [ "$EXPORT_RUNTIME_DEBUG" = "y" ]; then
     mkdir -vp $TARGET_DIR/binxa/debug
     cp -vf "$CUR_DIR"/binxa/ASEvaAPI.dll $TARGET_DIR/binxa/debug/
     cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binxa/debug/
-    if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+    if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/binxa/ASEvaAPIEto.dll $TARGET_DIR/binxa/debug/
         cp -vf "$CUR_DIR"/binxa/ASEvaAPIGtk.dll $TARGET_DIR/binxa/debug/
         cp -vf "$CUR_DIR"/binxa/PortableSharpGL.dll $TARGET_DIR/binxa/debug/
@@ -173,7 +173,7 @@ if [ "$EXPORT_RUNTIME_DEBUG" = "y" ]; then
     mkdir -vp $TARGET_DIR/binm/debug
     cp -vf "$CUR_DIR"/binm/ASEvaAPI.dll $TARGET_DIR/binm/debug/
     cp -vf "$CUR_DIR"/3party/common/* $TARGET_DIR/binm/debug/
-    if [ "$EXPORT_UI_LIBRARY" = "y" ]; then
+    if [ "$EXPORT_GUI_LIBRARY" = "y" ]; then
         cp -vf "$CUR_DIR"/binm/ASEvaAPIEto.dll $TARGET_DIR/binm/debug/
         cp -vf "$CUR_DIR"/binm/ASEvaAPIMonoMac.dll $TARGET_DIR/binm/debug/
         cp -vf "$CUR_DIR"/binm/PortableSharpGL.dll $TARGET_DIR/binm/debug/
