5046385b5baa7cb0ed5dd5c42e6e1ed9ac014626
Ken <sdken777@gmail.com>
2024-06-07 11:43:35 +0800

    gtk-3.2.2.1:修正X11OffscreenView的dummy box背景色问题

--- ASEvaAPIGtk/APIInfo.cs
+++ ASEvaAPIGtk/APIInfo.cs
@@ -31,7 +31,7 @@ namespace ASEva.UIGtk
         /// <returns>API版本</returns>
         public static Version GetAPIVersion()
         {
-            return new Version(3, 2, 2, 0); // Update log / 更新记录: 添加ShouldOverrideRunDialog
+            return new Version(3, 2, 2, 1); // Update log / 更新记录: 修正X11OffscreenView的dummy box背景色问题
         }
     }
 }
