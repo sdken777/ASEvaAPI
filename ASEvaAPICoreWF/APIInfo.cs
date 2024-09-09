ca0737c4f60f8371a3b5b42644df4baa2e77c48b
Ken <sdken777@gmail.com>
2024-09-09 11:38:48 +0800

    corewf-2.9.4.3:修正柱状折线图崩溃问题

--- ASEvaAPICoreWF/APIInfo.cs
+++ ASEvaAPICoreWF/APIInfo.cs
@@ -26,7 +26,7 @@ namespace ASEva.UICoreWF
         /// <returns>API版本</returns>
         public static Version GetAPIVersion()
         {
-            return new Version(2, 9, 4, 2); // Update log / 更新记录: 优化文件夹选择对话框标题显示
+            return new Version(2, 9, 4, 3); // Update log / 更新记录: 修正柱状折线图崩溃问题
         }
     }
 }
--- ASEvaAPICoreWF/HistLineGraph.Designer.cs
+++ ASEvaAPICoreWF/HistLineGraph.Designer.cs
@@ -52,7 +52,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.White;
             this.label2.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(0, 179);
+            this.label2.Location = new System.Drawing.Point(0, 180);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(0, 20);
             this.label2.TabIndex = 4;
