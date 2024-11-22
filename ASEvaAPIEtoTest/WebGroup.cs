8bde077c2c38ac58dceef7c4d440647128cc12fc
Ken <sdken777@gmail.com>
2024-11-22 16:34:33 +0800

    恢复EtoTest

--- ASEvaAPIEtoTest/WebGroup.cs
+++ ASEvaAPIEtoTest/WebGroup.cs
@@ -22,15 +22,6 @@ namespace ASEvaAPIEtoTest
             var buttonGo = layoutRow.AddButtonPanel(t["web-go-url"]);
             layoutRow.AddSeparator();
             layoutRow.AddButtonPanel(t["web-call-script"]).Click += delegate { webView.ExecuteScriptAsync("callScript()"); };
-            layoutRow.AddButtonPanel(t["web-exception-test"]).Click += delegate
-            {
-                try
-                {
-                    String str = null;
-                    str.Split(',');
-                }
-                catch (Exception) {}
-            };
 
             buttonGo.Enabled = false;
             buttonGo.DefaultBackgroundColor = Colors.LightGrey;
