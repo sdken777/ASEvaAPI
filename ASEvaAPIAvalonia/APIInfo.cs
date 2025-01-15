abfc077d7d485f27aac2ec1d0a20744738bd2e33
Ken <sdken777@gmail.com>
2025-01-15 09:37:32 +0800

    avalonia-1.2.9.2:修正hyperlink Button显示问题

--- ASEvaAPIAvalonia/APIInfo.cs
+++ ASEvaAPIAvalonia/APIInfo.cs
@@ -33,7 +33,7 @@ namespace ASEva.UIAvalonia
         /// <returns>API版本</returns>
         public static Version GetAPIVersion()
         {
-            return new Version(1, 2, 9, 1); // Update log / 更新记录: 简化样式引用
+            return new Version(1, 2, 9, 2); // Update log / 更新记录: 修正hyperlink Button显示问题
         }
 
         /// \~English
