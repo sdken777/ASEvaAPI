90f51c6aacddd7ca7e1fdc188fb7e8a9e182d56a
Ken <sdken777@gmail.com>
2025-02-13 13:45:53 +0800

    common-3.7.10:新增TaskBeat.MinInterval

--- ASEvaAPI/Utility/TaskBeat.cs
+++ ASEvaAPI/Utility/TaskBeat.cs
@@ -13,9 +13,18 @@ namespace ASEva.Utility
 	/// </summary>
     public class TaskBeat
     {
+        /// \~English
+        /// <summary>
+        /// Loop call this method
+        /// </summary>
+        /// \~Chinese
+        /// <summary>
+        /// 循环调用此方法
+        /// </summary>
         public async void Handle(Func<Task> task)
         {
             if (curAsyncTask != null) return;
+            if (intervalControl != null && !intervalControl.Test()) return;
 
             try
             {
@@ -31,7 +40,29 @@ namespace ASEva.Utility
             catch (Exception ex) { Dump.Exception(ex); }
             curAsyncTask = null;
         }
+        
+        /// \~English
+        /// <summary>
+        /// (api:app=3.7.10) The minimum interval between task executions, in seconds
+        /// </summary>
+        /// \~Chinese
+        /// <summary>
+        /// (api:app=3.7.10) 任务执行的最小间隔，单位秒
+        /// </summary>
+        public double MinInterval
+        {
+            get => minInterval;
+            set
+            {
+                if (value < 0 || value == minInterval) return;
+                minInterval = value;
+                if (minInterval == 0) intervalControl = null;
+                else intervalControl = new IntervalControl(minInterval, false, false);
+            }
+        }
 
         private Task curAsyncTask = null;
+        private double minInterval = 0;
+        private IntervalControl intervalControl = null;
     }
 }
